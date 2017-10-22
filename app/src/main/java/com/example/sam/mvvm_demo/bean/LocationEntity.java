6e560234170b1349c1577fb9f18ff865a727d9a0
sam456852 <weixiangzhang@u.northwestern.edu>
2017-10-22 01:50:26 -0500

    Initialize

---
+++ app/src/main/java/com/example/sam/mvvm_demo/bean/LocationEntity.java
@@ -0,0 +1,8 @@
+package com.example.sam.mvvm_demo.bean;
+
+/**
+ * Created by sam on 10/22/17.
+ */
+
+public class LocationEntity {
+}
---
+++ app/src/main/java/com/example/sam/mvvm_demo/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sam.mvvm_demo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sam.mvvm_demo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
